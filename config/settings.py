5a82de02a48f25a5b737d1a672c5436eb40d2f9c
Kenji. <147786329+JL-codecreator@users.noreply.github.com>
2025-04-03 08:54:10 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -196,10 +196,10 @@ EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # Your Gmail email
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # App Password
-EMAIL_FROM_ADDRESS = config("EMAIL_FROM_ADDRESS", default=EMAIL_HOST_USER)  # Fix here
-DEFAULT_FROM_EMAIL = EMAIL_FROM_ADDRESS
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+
 
 # crispy config
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
